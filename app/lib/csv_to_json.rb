76c4c414d027aa981cffad8a354c54545650b03b
Michael Ho <chacharoti@gmail.com>
2019-12-10 22:23:24 -0800

    Validate file name

--- app/lib/csv_to_json.rb
+++ app/lib/csv_to_json.rb
@@ -5,10 +5,18 @@ require_relative '../lib/employee'
 require_relative '../lib/converter/csv'
 
 puts 'Begin'
-csv_file_converter = Converter::Csv.new 'app/lib/csv_files/sample.csv'
-csv_file_converter.convert_to_json
-if csv_file_converter.errors.any?
-  puts csv_file_converter.errors.join('. ')
+puts 'Please input file name (eg: sample.csv)'
+puts 'Please note: File must be at app/lib/csv_files):'
+file_name = gets.chomp
+path = 'app/lib/csv_files/' + file_name
+if file_name!= '' && !file_name.nil? && (File.exist? path)
+  csv_file_converter = Converter::Csv.new path
+  csv_file_converter.convert_to_json
+  if csv_file_converter.errors.any?
+    puts csv_file_converter.errors.join('. ')
+  end
+  puts "File convert succsessfull. Please find json file at: #{csv_file_converter.result_path}"
+else
+  puts 'Invalid file name.'
 end
-puts "File convert succsessfull. Please check json at: #{csv_file_converter.result_path}"
 
